744abac0f9f867cf2e5813800ff57966223e3ef1
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-24 23:25:08 +0100

    add server file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const gameRoutes = require('./backend/routes/gameRoutes');
+
+const app = express();
+app.use(express.json());
+app.use('/games', gameRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+});
