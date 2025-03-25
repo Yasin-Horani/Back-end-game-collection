ce824e0825e9a626ceb9d7d88f39651cc023762e
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-25 02:26:33 +0100

    modify the server file

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const gameRoutes = require('./backend/routes/gameRoutes');
 
 const app = express();
-app.use(express.json());
+
+// Increase request size limit to 10MB
+app.use(express.json({ limit: "10mb" }));  
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));  
+
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
 app.use('/games', gameRoutes);
 
 const PORT = 5500;
